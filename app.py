63647e32a4a84171f623445e499729a262f33232
Pranav Garg <pranavgarg@Pranavs-MacBook-Pro.local>
2025-07-13 15:14:43 +0800

    Initial frontend commit

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def view_data(location):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
